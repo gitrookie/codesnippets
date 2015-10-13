3311b86fec8c515b7150b6b5276852aef63bd75d
Gaurav Sood <gaurav3184@gmail.com>
2015-10-13 13:28:33 +0530

    [Initial Commit to codesnippet repo]

---
+++ pycode/test.py
@@ -0,0 +1,9 @@
+def f2(n):
+    r = []
+    for i, c in enumerate(reversed(str(n))):
+        if i and (not (i % 3)):
+            r.insert(0, ',')
+        r.insert(0, c)
+    return "".join(r)
+
+print(f2(1234567))
