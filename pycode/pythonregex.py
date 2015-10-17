223d102af70a3e54bbe8ce5974ae4280cc1272c5
Gaurav Sood <gaurav.sood031@gmail.com>
2015-10-17 22:04:32 +0530

    [Added some more files]

--- pycode/pythonregex.py
+++ pycode/pythonregex.py
@@ -9,9 +9,8 @@ print(match.group(1))
 patt = re.compile("<(.*)>", re.IGNORECASE)
 match = patt.search("<h1>Introduction</h1>")
 
-
-
 # Alternation
+
 patt = re.compile("(abcde|abc)", re.IGNORECASE)
 match = patt.search("abcde")
 
@@ -22,7 +21,18 @@ patt = re.compile("(?P<name>abcd)\s(?P=name)", re.IGNORECASE)
 # patt = re.compile("(abcd) \1", re.IGNORECASE)
 match = patt.search("abcd abcde")
 
+# Thousand separator
+thou = re.sub("\d(?=(\d{3})+(?!\d))", r"\1,", "123456789")
+patt = re.compile("(\d)(?=(\d{3})+(?!\d))")
+match = patt.search("123456789")
 
-thou = re.sub("(\d)(?=(\d{3})+(?!\d))", r"\1,", "123456789")
-# patt = re.compile("(\d)(?=(\d{3})+(?!\d))")
-# match = patt.search("12345678")
+patt = re.compile(r"\b([a-z]+)\s+\1\b", re.IGNORECASE)
+match = patt.search("hey the the issue is serious")
+s = re.sub(patt, r"\1", "hey the The issue")
+
+
+pos = re.compile("$", re.IGNORECASE)
+match = pos.search("hi")
+print(match)
+s = re.sub(pos, " there", "hi")
+print(s)
--- pycode/test.py
+++ pycode/test.py
@@ -1,4 +1,16 @@
 # Adding thousand separator in a number
+import time
+
+
+def Timer(f):
+    def wrapper(n):
+        start = time.time()
+        f(n)
+        return time.time() - start
+    return wrapper
+
+
+@Timer
 def f2(n):
     r = []
     for i, c in enumerate(reversed(str(n))):
@@ -7,9 +19,24 @@ def f2(n):
         r.insert(0, c)
     return "".join(r)
 
-print(f2(1234567))
+# print(f2(1234567))
 
 # Using str's format method
+
+
+@Timer
 def f1(n):
     return "{0:,}".format(n)
-    
+
+import re
+patt = re.compile("(\d)(?=(\d{3})+(?!\d))")
+
+
+@Timer
+def f3(n):
+    # patt = re.compile("(\d)(?=(\d{3})+(?!\d))")
+    return re.sub(patt, r'\1,', n)
+
+print(f2(str(123456789)))
+print(f1(123456789))
+print(f3(str(123456789)))
