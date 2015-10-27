f937e8f33c49d48d319fff8cf5e4626ec87ec73b
Gaurav Sood <gaurav3184@gmail.com>
2015-10-27 11:08:39 +0530

    [Changes to pythreads]

--- pycode/pythreads.py
+++ pycode/pythreads.py
@@ -6,10 +6,15 @@ import threading
 # args is argument tuple for target invocation
 
 
+'''
 class MyThread(threading.Thread):
     def start(self):
         print("In start")
         super().start()
+
+    def run(self):
+        print("In run method")
+        super().run()
 
 lock = threading.Lock()
 
@@ -39,24 +44,35 @@ t2.start()
 
 # t1.join()
 # t2.join()
-
 '''
-state = 0
+
+# '''
+# state = 0
 
 
-def func():
-    global state
-    print("Current Thread is")
-    state += 1
+# def func():
+#     global state
+#     print("Current Thread is")
+#     state += 1
 
-t1 = threading.Thread(target=func)
-t2 = threading.Thread(target=func)
+# t1 = threading.Thread(target=func)
+# t2 = threading.Thread(target=func)
 
-t1.start()
-t2.start()
+# t1.start()
+# t2.start()
 
-t1.join()
-t2.join()
+# t1.join()
+# t2.join()
 
-print(state)
-'''
+# print(state)
+# '''
+
+#####################################################################
+################## Queues ###########################################
+#####################################################################
+import queue
+# queue module implements multiproducer and multiconsumer queues.
+# queue module provides multiproducer and multiconsumer implementation suitable
+# for multithreading. queue.Queue class implements all locking semantics. It
+# depends upon the availability of thread support in python. Provides
+# FIFO(queue.Queue), LIFO, priority queues.
